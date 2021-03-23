68a17ca9e73235a49c1b80226a754b0818cfd7a3
mmmG <a331998513@gmail.com>
2021-03-23 19:34:22 +0100

    99%

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -26,10 +26,12 @@ const addTodo = (event)=>{
     
     //append it
     todoTxtInput.value = todoInput.value;
+    localTodoSave(todoInput.value);
     todoLi.appendChild(todoTxtInput);
     todoLi.appendChild(todoBtn1);
     todoLi.appendChild(todoBtn2);
     todoUl.appendChild(todoLi);
+    
     todoInput.value = '';
     }
 }
@@ -39,6 +41,7 @@ const deleteTodo = (event) =>{
     let item = event.target;
     if (item.classList[0] == 'list-btn2'){
     item.closest('.list-item').classList.toggle('fall')
+    removeLocalTodo(item)
      const remove = () =>{item.closest('.list-item').remove()}
      setTimeout(remove, 700)
     }
@@ -70,7 +73,65 @@ const filterTodo = (event)=>{
         }
     })
 }
+//save data to local
+const localTodoSave = (data) =>{
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(data);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+//get data from local
+
+const getLocalTodo = () =>{
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    }   else {
+        todos= JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach((data)=>{
+        const todoLi = document.createElement('LI');
+        todoLi.classList.add('list-item');
+    
+        const todoTxtInput = document.createElement('INPUT');
+        todoTxtInput.disabled = true;
+        todoTxtInput.classList.add('list-text');
+        
+        const todoBtn1 = document.createElement('BUTTON');
+        todoBtn1.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+        todoBtn1.classList.add('list-btn1');
+        
+        const todoBtn2 = document.createElement('BUTTON');
+        todoBtn2.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+        todoBtn2.classList.add('list-btn2');
+        
+        //append it
+        todoTxtInput.value = data;
+        todoLi.appendChild(todoTxtInput);
+        todoLi.appendChild(todoBtn1);
+        todoLi.appendChild(todoBtn2);
+        todoUl.appendChild(todoLi);
+    })
+}
+const removeLocalTodo = (data) =>{
+    let todos;
+    if(localStorage.getItem('todos' === null)){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    const theValue = data.closest('li').children[0].value
+    console.log(theValue);
+    console.log(todos.indexOf(theValue));
+    todos.splice(todos.indexOf(theValue),1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 //event listener
+document.addEventListener('DOMContentLoaded',getLocalTodo)
 todoBtn.addEventListener('click', addTodo);
 todoUl.addEventListener('click', deleteTodo)
 optionFilter.addEventListener('click', filterTodo)
